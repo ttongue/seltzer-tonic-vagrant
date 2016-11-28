de836dc335d2b29e5bfba90a0b47e5c04ff24780
Thomas Tongue <tom.tongue@gmail.com>
2016-11-27 21:33:30 -0500

    Added the symbolic links and file structures to support loading the separate packages for seltzer and tonic and any future code.

---
+++ tvcog-systems/install_scripts/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /var/www/html
+ln -s ../../tvcog-systems/cardsystem .
+ln -s ../../tvcog-systems/crm .
+ln -s ../../tvcog-systems/kastellan .
+cd /var/wwws/html
+ln -s ../../tvcog-systems/billing .
+ln -s ../../tvcog-systems/forms .
+
